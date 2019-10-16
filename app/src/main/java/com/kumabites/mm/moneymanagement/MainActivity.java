982498ed44563490df27e1a2c232815aa0f85fbf
KumaBites <53787632+KumaBites@users.noreply.github.com>
2019-10-16 14:29:26 +0100

    Changed so that log out button and back button is same plac eon all screens, and fix minor bugs -log in system fully was not capturing user input -added label to register user page -solved bug which didn't allow new debts to be created

--- app/src/main/java/com/kumabites/mm/moneymanagement/MainActivity.java
+++ app/src/main/java/com/kumabites/mm/moneymanagement/MainActivity.java
@@ -1,10 +1,12 @@
 package com.kumabites.mm.moneymanagement;
 
 import MMENTITY.User;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -25,12 +27,16 @@ public class MainActivity extends AppCompatActivity {
     public static AppDatabase appDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          user = (EditText)findViewById(R.id.userText);
          pass =(EditText)findViewById(R.id.passwordText);
         appDatabase = Room.databaseBuilder(getApplicationContext(),AppDatabase.class,"userdb").allowMainThreadQueries().fallbackToDestructiveMigration().build();
     }
+
     public void registerNew(View view){
         Intent rIntent = new Intent(this, NewUser.class);
         startActivity(rIntent);
@@ -81,19 +87,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void exit(View view){
-        System.exit(0);
+        confirmLogOutDialog();
     }
 
     //deletes all data from database
     public void nuketable(View view){
-        List<User> check = MainActivity.appDatabase.userDao().getAnyUser();
-        if(check.isEmpty())
-        {Toast.makeText(getBaseContext(),"There are no users to delete!",Toast.LENGTH_SHORT).show();}
-        else{
-        appDatabase.clearAllTables();
-
-        Toast.makeText(getBaseContext(),"All Data Deleted!!",Toast.LENGTH_SHORT).show();
-    }}
+        confirmDeleteDialog();
+}
     //checks the input to see if user is registered
     public void oldUser(View view) {
         oldUser = user.getText().toString();
@@ -105,7 +105,58 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private void confirmDeleteDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm Delete Action!");
+        builder.setMessage("You are about to delete all records of database. Do you really want to proceed ?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "You've choosen to delete all records", Toast.LENGTH_SHORT).show();
+                List<User> check = MainActivity.appDatabase.userDao().getAnyUser();
+                if(check.isEmpty())
+                {Toast.makeText(getBaseContext(),"There are no users to delete!",Toast.LENGTH_SHORT).show();}
+                else{
+                    appDatabase.clearAllTables();
 
+                    Toast.makeText(getBaseContext(),"All Data Deleted!!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "You've changed your mind to delete all records", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void confirmLogOutDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm Log Out Action");
+        builder.setMessage("You are about to exit the app. Do you really want to proceed ?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "You've logged out!", Toast.LENGTH_SHORT).show();
+                System.exit(0);
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "You've changed your mind to logged out", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.show();
+    }
 
 }
 
--- app/src/main/java/com/kumabites/mm/moneymanagement/Pay/confirmPay.java
+++ app/src/main/java/com/kumabites/mm/moneymanagement/Pay/confirmPay.java
@@ -1,9 +1,12 @@
 package com.kumabites.mm.moneymanagement.Pay;
 
 import MMENTITY.Debt;
+import MMENTITY.User;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,24 +24,24 @@ public class confirmPay extends AppCompatActivity {
     private TextView payDebt, confirmPay, debtName;
     private int pText, absolutePText, debtRemain, debtPay;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_pay);
         debtName = (TextView) findViewById(R.id.debtName);
         confirmPay= (TextView)findViewById(R.id.confirmPay);
+        payDebt = (TextView)findViewById(R.id.confirmName);
         Intent confirm = getIntent();
-
-
         newDebtName = confirm.getStringExtra("Debt Name");
-        debtName.setText("How much do you want to pay to debt : " + newDebtName + "?");
-
+        debtName.setText("How much are you paying off ");
+        payDebt.setText(newDebtName+" ?");
 
     }
+    public void confirmedPay(View view){
+        confirmPayDialog();
 
-    public void payDebt(View view) {
+    }
+    public void payDebt() {
         String newDText =  newDebtName;
         String newPText = confirmPay.getText().toString();
         if (newPText.equals("")) {
@@ -76,14 +79,36 @@ public class confirmPay extends AppCompatActivity {
         }
     }
 
+    private void confirmPayDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm Payment");
+        builder.setMessage("You are about to pay a debt. Do you really want to proceed ?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                payDebt();
 
+
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "You've cancelled payment", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.show();
+    }
 
     public void goBack(View view){
-
         Intent goBack = new Intent(this,PayDebt.class);
         startActivity(goBack);
         finish();
     }
+
 
     @SuppressLint("MissingSuperCall")
     @Override
